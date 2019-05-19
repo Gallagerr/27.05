a4e8b60c432248e91db1e2f77d400ec42a225a4d
Gallagerr <46028442+Gallagerr@users.noreply.github.com>
2019-05-19 14:06:07 +0600

    Добавьте файлы проекта.

---
+++ 26.05/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _26._05
+{
+  /// <summary>
+  /// Логика взаимодействия для MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+    {
+      PrintDialog printDialog = new PrintDialog();
+      if (printDialog.ShowDialog() == true)
+      {
+        printDialog.PrintVisual(FDR, "Printing in process");
+      }
+    }
+  }    
+}
+
